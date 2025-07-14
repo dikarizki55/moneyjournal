ef6ef2ca34b82aabc5906c9a31bc8c01ee651e82
dikarizki55 <dikaesmidorikawa55@gmail.com>
2025-07-14 15:29:47 +0700

    test verifyUser

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,5 +1,12 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { verifyUser } from "@/lib/verifyuser";
 
-export async function GET() {
-  return NextResponse.json({ message: "halooo" });
+export async function GET(req: NextRequest) {
+  try {
+    const user = await verifyUser(req);
+
+    return NextResponse.json({ message: "halooo", data: user });
+  } catch (error) {
+    return NextResponse.json({ message: "error" }, { status: 401 });
+  }
 }
